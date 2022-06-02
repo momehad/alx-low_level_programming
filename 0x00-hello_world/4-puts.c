a9924fad2ee3c162184d2ebf126683a1e8e6d91f
Mohammed Mehad <mexspark17@gmail.com>
2022-06-02 18:05:39 +0300

    complete

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
 /**
- * prints "\"Programming is like building a multilingual puzzle"
- * Return:0
+ * main - prints "\"Programming is like building a multilingual puzzle"
+ * Return:0 
  */
 int main(void)
 {
