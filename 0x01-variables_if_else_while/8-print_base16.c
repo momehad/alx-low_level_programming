bc2c0c358cf9a3e51631af09d1ccde2d2456fa01
Mohammed Mehad <mexspark17@gmail.com>
2022-06-04 02:12:37 +0300

    Done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-    char n;
-    for (n = '0'; n <= '9'; n++)
-        putchar(n);
-    for (n = 'a'; n <= 'f'; n++)
-        putchar(n);
-    putchar('\n');
-    return (0);
+char n;
+for (n = '0' ; n <= '9' ; n++)
+putchar (n);
+for (n = 'a' ; n <= 'f' ; n++)
+putchar (n);
+putchar ('\n');
+return (0);
 }
