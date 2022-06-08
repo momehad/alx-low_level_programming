e603c2f773396033847fae21d43c98965b136921
Mohammed Mehad <mexspark17@gmail.com>
2022-06-08 14:40:55 +0300

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "main.h"
+#include <stdlib.h>
 /**
  * main - Entry point
  * Description: prints '_putchar \n'
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-char str[] = "_putchar\n";
+char c[10] = "Holberton";
 int i = 0;
-
-while (str[i] != '\0')
-	{
-	char c = str[i];
-
-	_putchar(c);
-	i++;
-	}
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
 return (0);
 }
