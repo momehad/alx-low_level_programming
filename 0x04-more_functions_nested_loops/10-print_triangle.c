dd5a4be9408fe5724baef156f6d021d0df70736b
Mohammed Mehad <mexspark17@gmail.com>
2022-06-10 18:13:04 +0300

    Complete

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,16 @@
  */
 void print_triangle(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
+	int i = 1, ii;
 
-		for (i = 1; i <= size; i++)
+	while (i <= size && size > 0)
+	{
+		ii = 0;
+		while (ii < size - i)
 		{
-			for (j = i; j < size; j++)
-			{
-				_putchar(' ');
-			}
+			_putchar(' ');
+			ii++;
+		}
 
 			for (j = 1; j <= i; j++)
 			{
